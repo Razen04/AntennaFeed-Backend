8c607f7cae0639b42e348374abf4694250f48ce1
Razen <09876sp54321@gmail.com>
2025-01-05 15:43:08 +0530

    added / response

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ app.use('/articles', articleRoutes);
 app.use('/feeds', feedRoutes);
 app.use('/opml', opmlRoutes);
 
+// For cron-job
+app.get('/', (req, res) => {
+    res.send("Server is up and running.");
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
